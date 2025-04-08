351ef0ead26186a57bc800cbf1f3b2633ba4fc5b
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-04-09 02:29:30 +0530

    Updated styles, README, index.html, and added Google site verification

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google site verification meta tag */}
+        <meta name="google-site-verification" content="1A1fRmjF9nSwigUsFrBH9cy43Hg9TIcDEHZGJ7vL6GI" />
+        {/* Add other meta tags or links here */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
